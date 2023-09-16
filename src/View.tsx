effb5332ac6f3c59459b1f0b67e4c0d1acbd34c6
slk333 <aweb33@icloud.com>
2023-09-16 18:29:12 +0200

    pokemon now have their correct special stats. stats display ui

--- src/View.tsx
+++ src/View.tsx
@@ -1,23 +1,34 @@
 import { Flex, Space } from "@mantine/core"
-import pokemons from "./data/pokemons.json"
+import { pokemons } from "./data/pokemons.ts"
 
 export default function View() {
     return (
         <div>
-            <Space h={10}></Space>
-            {pokemons.map(pokemon => (
-                <Flex gap={10} align={"center"}>
-                    <div>{pokemon.id}</div>
-
-                    <img src={pokemon.spriteURL} width={96}></img>
-                    <div>{pokemon.name}</div>
-                    <div>
-                        {/*   {pokemon.types.map(type => (
+            <Flex direction="column" gap={30}>
+                {pokemons.map(pokemon => (
+                    <Flex key={pokemon.id} gap={10} align={"center"}>
+                        <div>{pokemon.id}</div>
+                        <img src={pokemon.spriteURL} width={96}></img>
+                        <Flex direction={"column"}>
+                            <meter style={{ width: "300px" }} min={0} max={200} value={pokemon.stats[0]}></meter>
+                            <meter style={{ width: "300px" }} min={0} max={200} value={pokemon.stats[1]}></meter>
+                            <meter style={{ width: "300px" }} min={0} max={200} value={pokemon.stats[2]}></meter>
+                            <meter style={{ width: "300px" }} min={0} max={200} value={pokemon.stats[3]}></meter>
+                            <meter style={{ width: "300px" }} min={0} max={200} value={pokemon.stats[4]}></meter>
+                            <meter
+                                style={{ width: "300px", backgroundColor: "#777" }}
+                                min={0}
+                                max={1000}
+                                value={pokemon.stats.reduce((a, b) => a + b)}
+                            ></meter>
+                            {/*   {pokemon.types.map(type => (
                             <span>{type}</span>
                         ))} */}
-                    </div>
-                </Flex>
-            ))}
+                        </Flex>
+                        <div>{pokemon.name}</div>
+                    </Flex>
+                ))}
+            </Flex>
         </div>
     )
 }
