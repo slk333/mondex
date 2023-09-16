95b0443ef5567b704a83c89bc2dafe1c137650c7
Antoine Weber <aweb33@icloud.com>
2023-09-16 17:00:54 +0200

    was missing some files

--- src/View.tsx
+++ src/View.tsx
@@ -1,14 +1,23 @@
-import { Button, Center, Space } from "@mantine/core"
+import { Flex, Space } from "@mantine/core"
+import pokemons from "./data/pokemons.json"
 
 export default function View() {
     return (
         <div>
             <Space h={10}></Space>
-            <Center>
-                <Button variant="default" color="indigo">
-                    Hello
-                </Button>
-            </Center>
+            {pokemons.map(pokemon => (
+                <Flex gap={10} align={"center"}>
+                    <div>{pokemon.id}</div>
+
+                    <img src={pokemon.spriteURL} width={96}></img>
+                    <div>{pokemon.name}</div>
+                    <div>
+                        {/*   {pokemon.types.map(type => (
+                            <span>{type}</span>
+                        ))} */}
+                    </div>
+                </Flex>
+            ))}
         </div>
     )
 }
